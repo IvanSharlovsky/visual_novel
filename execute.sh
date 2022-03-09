cb0347c1d4e0a760750e66d26db40138e90dc962
timkingteam <92477489+timkingteam@users.noreply.github.com>
2022-03-10 02:10:31 +0300

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
 git clone git@github.com:IvanSharlovsky/visual_novel.git
 RENPY_PATH=$(find 2> /dev/null ~ -name renpy-7.4.11-sdk)
-mv 2> /dev/null $RENPY_PATH/visual-novel $RENPY_PATH/visual-novel-old
-mv 2> /dev/null $RENPY_PATH/visual-novel-old $RENPY_PATH/visual-novel-old2
+mv 2> /dev/null $RENPY_PATH/visual-novel-old6 $RENPY_PATH/visual-novel-old7
+mv 2> /dev/null $RENPY_PATH/visual-novel-old5 $RENPY_PATH/visual-novel-old6
+mv 2> /dev/null $RENPY_PATH/visual-novel-old4 $RENPY_PATH/visual-novel-old5
+mv 2> /dev/null $RENPY_PATH/visual-novel-old3 $RENPY_PATH/visual-novel-old4
 mv 2> /dev/null $RENPY_PATH/visual-novel-old2 $RENPY_PATH/visual-novel-old3
-cp -r visual_novel/visual-novel $RENPY_PATH
-open $RENPY_PATH/renpy.app
+mv 2> /dev/null $RENPY_PATH/visual-novel-old1 $RENPY_PATH/visual-novel-old2
+mv 2> /dev/null $RENPY_PATH/visual-novel $RENPY_PATH/visual-novel-old1
+mv visual_novel/visual-novel $RENPY_PATH
+rm -r visual_novel
+open ${RENPY_PATH}/renpy.app
